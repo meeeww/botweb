e06afa1c9971e7846b8d6cf2d20b74b4631cf061
DESKTOP-C9HHN7V\A8-PC00 <licencias@svalero.com>
2023-02-21 16:39:28 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Header from "./componentes/Header"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="w-screen">
+      <Header />
+    </div>
+  )
+}
+
+export default App
